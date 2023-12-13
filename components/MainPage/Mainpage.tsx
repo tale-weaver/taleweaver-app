e9fb6165661290afc078a151624a2b25c044413c
r11725018 <46022223+mikemikemike451@users.noreply.github.com>
2023-12-13 11:29:40 +0800

    mainpage

---
+++ components/MainPage/Mainpage.tsx
@@ -0,0 +1,19 @@
+// app/page.tsx
+'use client'
+import Image from 'next/image'
+import Intro from './Intro'
+import Bar from './Bar'
+
+const  Mainpage = ({}) =>{
+  return ( 
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div><Intro/></div>
+      <br></br>
+      <div id="finish"><Bar section="已完成故事"/></div>
+      <br></br>
+      <div id="unfinish"><Bar section="未完成故事"/></div>
+    </main>
+  )
+}
+export default Mainpage
+
