f13a9fc251b9cd1eb80777c14d9ea9f7c7969be7
hc-psy <austenpsy@gmail.com>
2023-12-16 14:07:56 +0000

    [fix] main page layout a bit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import { options } from "./api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth/next";
-import Mainpage from '@/components/MainPage/Mainpage'
+import Mainpage from "@/components/MainPage/Mainpage";
 
 export default async function HomePage() {
-  const session = await getServerSession(options);
-  return (
-    <div>
-      <Mainpage/>
-      {session ? (
-        <p>Logged in as {session?.user?.name}</p>
-      ) : (
-        <p>Not logged in</p>
-      )}
-    </div>
-  );
+  return <Mainpage />;
 }
-
