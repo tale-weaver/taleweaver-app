12a8a6d63e6415be70fad52b38985ff25f7c24b0
hc-psy <austenpsy@gmail.com>
2023-12-16 14:52:29 +0000

    [feat] finally page turner

--- components/header/user-auth.tsx
+++ components/header/user-auth.tsx
@@ -4,21 +4,10 @@ import { Button } from "../ui/button";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { removeAuthHeader } from "@/lib/api/common";
-import { useSession } from "next-auth/react";
-import axios from "axios";
 
 const UserAuth = ({
   imgUrl = "https://gravatar.com/avatar/b6eada68a0879b7af38a4ddf32e06aa7?s=400&d=robohash&r=x",
 }) => {
-  const { data: session } = useSession();
-
-  if (session?.user) {
-    console.log("token", (session?.user as any)?.accessToken);
-    axios.defaults.headers.common["Authorization"] = `Bearer ${
-      (session?.user as any)?.accessToken
-    }`;
-  }
-
   const handleSignOut = () => {
     removeAuthHeader();
     signOut();
--- components/storypage/page-veiwer.tsx
+++ components/storypage/page-veiwer.tsx
@@ -8,8 +8,6 @@ import { Pages } from "@/data/pages";
 export default function PageVeiwer() {
   const index = useRef(0);
   const [ref, { width }] = useMeasure();
-
-  console.log(width);
   const [props, api] = useSprings(
     Pages.length,
     (i) => ({
@@ -33,31 +31,32 @@ export default function PageVeiwer() {
         if (i < index.current - 1 || i > index.current + 1) {
           return { display: "none" };
         }
-        console.log("i", i);
         const x = (i - index.current) * width + (active ? mx : 0);
-        const scale = active ? 1 - distance / width / 2 : 1;
+        const scale = active ? 1 - distance / width / 1 : 1;
         return { x, scale, display: "block" };
       });
     }
   );
   return (
-    <div ref={ref} className="pt-20 w-[250px] h-[330px] overflow-hidden z-40">
-      {props.map(({ x, display, scale }, i) => (
-        <animated.div
-          className="absolute w-[250px] h-[330px] will-change-transform"
-          {...bind()}
-          key={i}
-          style={{ display, x }}
-        >
+    <div className="my-20 w-[250px] h-[330px] overflow-hidden relative">
+      <div ref={ref} className="w-[250px] h-[330px] absolute">
+        {props.map(({ x, display, scale }, i) => (
           <animated.div
-            className="touch-none bg-cover bg-no-repeat bg-center w-[250px] h-[330px] will-change-transform shadow-[0_62.5px_125px_-25px_rgba(50,50,73,0.5),0_37.5px_75px_-37.5px_rgba(0,0,0,0.6)]"
-            style={{
-              scale,
-              backgroundImage: `url(${Pages[i].image})`,
-            }}
-          />
-        </animated.div>
-      ))}
+            className="absolute w-[250px] h-[330px] will-change-transform"
+            {...bind()}
+            key={i}
+            style={{ display, x }}
+          >
+            <animated.div
+              className="touch-none bg-cover bg-no-repeat bg-center w-[250px] h-[330px] will-change-transform shadow-[0_62.5px_125px_-25px_rgba(50,50,73,0.5),0_37.5px_75px_-37.5px_rgba(0,0,0,0.6)]"
+              style={{
+                scale,
+                backgroundImage: `url(${Pages[i].image})`,
+              }}
+            />
+          </animated.div>
+        ))}
+      </div>
     </div>
   );
 }
