488ef97dee95e89cf1de9165e2c4a2d3dd06a22d
hc-psy <austenpsy@gmail.com>
2023-12-19 09:27:40 +0000

    [fix] avatar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { FC, ReactNode } from "react";
-import AnimationWapper from "@/components/wappers/AnimationWapper";
+
 interface AuthLayoutProps {
   children: ReactNode;
 }
@@ -9,9 +7,7 @@ interface AuthLayoutProps {
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
   return (
     <div className="h-[calc(100vh-161px)] flex flex-col justify-center items-center">
-      <div className="bg-white rounded-md">
-        <AnimationWapper>{children}</AnimationWapper>
-      </div>
+      <div className="bg-white rounded-md">{children}</div>
     </div>
   );
 };
--- app/story/[book_id]/page.tsx
+++ app/story/[book_id]/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import DisplayInfo from "@/components/bookid/DisplayInfo";
-
+import StoryPageMain from "@/components/storypage/story-page-main";
 
 export default function StoryView() {
-
-    //回傳的資訊除了網址上的ID，其他都是假的
-    return (
-        <div>
-            <DisplayInfo />
-
-        </div>
-    )
+  //回傳的資訊除了網址上的ID，其他都是假的
+  return (
+    <div>
+      <DisplayInfo />
+      <StoryPageMain />
+    </div>
+  );
 }
