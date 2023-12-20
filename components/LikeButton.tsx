6dc1e2d263dcae1111e8668de256837e475a8066
Chu <samasa112233@gmail.com>
2023-12-20 10:42:45 +0800

    Like button temp

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -5,6 +5,7 @@ import { Button, buttonVariants } from '@/components/ui/button'
 import { Heart } from "lucide-react";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 interface LikeButtonProps {
   bookId: string; // 傳給後端的 book_id
@@ -14,7 +15,7 @@ interface LikeButtonProps {
 
 const LikeButton: React.FC<LikeButtonProps> = ({ bookId, liked, like_nums }) => {
   const [likeCount, setLikeCount] = useState(like_nums);
-  const [isFilledHeart, setIsFilledHeart] = useState(liked);
+  // const [isFilledHeart, setIsFilledHeart] = useState(liked);
 
   const [endpointParam, setEndpointParam] = useState(bookId);
 
@@ -23,14 +24,27 @@ const LikeButton: React.FC<LikeButtonProps> = ({ bookId, liked, like_nums }) => 
 
   useEffect(() => {
     setLikeCount(like_nums);
-    setIsFilledHeart(liked)
+    // setIsFilledHeart(liked)
     
     if (bookId == undefined) {
       setEndpointParam(params);
     }
-  }, [like_nums, liked])
+  }, [like_nums]) //, liked])
 
   // console.log("Receive the params:", endpointParam, liked, like_nums)
+
+  // const queryClient = useQueryClient();
+
+  // const { data } = useQuery(
+  //   ['story', endpointParam],
+  //   async () => {
+  //     const response = await axios.get(`http://127.0.0.1:5000/story/${endpointParam}`);
+  //     return response.data;
+  //   },
+  //   {
+  //     initialData: { records: { numlikes: like_nums } }, // Set initial data based on the current like count
+  //   }
+  // );
 
   const handleLike = async () => {
     try {
@@ -45,7 +59,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({ bookId, liked, like_nums }) => 
         // console.log(data)
 
         setLikeCount(data.records.numlikes);
-        setIsFilledHeart(prev => !prev);
+        // setIsFilledHeart(prev => !prev);
 
         const router = useRouter();
         router.refresh();
@@ -61,8 +75,11 @@ const LikeButton: React.FC<LikeButtonProps> = ({ bookId, liked, like_nums }) => 
   return (
     <div className="flex items-center">
       {/* like = add to user collection; unlike = remove from user collection */}
-      <Button variant={isFilledHeart ? "destructive" : "outline"} onClick={handleLike}>
+      {/* <Button variant={isFilledHeart ? "destructive" : "outline"} onClick={handleLike}>
         <Heart color={isFilledHeart ? "white" : "black"} />
+      </Button> */}
+      <Button variant="outline" onClick={handleLike}>
+        <Heart color="black" />
       </Button>
       <span className="pl-2"> {likeCount}</span>
     </div>
